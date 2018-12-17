3f5797f4755a4693d2c57f4a2339a6bd20596b03
fbisquerra <fbisquerra@hiberus.es>
2018-12-17 12:59:59 +0100

    Add typeahead component and button

--- src/components/Forecast.vue
+++ src/components/Forecast.vue
@@ -1,19 +1,45 @@
 <template>
   <div>
-   Forecast view
+    <div class="row">
+      <div class="col-5 offset-3">
+        <type-ahead v-model="city" :src="citiesUrl" :getResponse="getResponse"></type-ahead>
+      </div>
+      <div class="col-1">
+        <button type="button" class="btn btn-primary" v-on:click="fetchForecast()">Search</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Forecast',
-  props: {
+import { BASE_URL } from "../backend.js";
+import TypeAhead from "vue2-typeahead";
 
+export default {
+  name: "Forecast",
+  data() {
+    return {
+      cities: [],
+      city: "",
+      citiesUrl: `${BASE_URL}/api/cities`
+    };
+  },
+  mounted: function() {
+    this.baseUrl = BASE_URL;
+  },
+  components: {
+    "type-ahead": TypeAhead
+  },
+  methods: {
+    getResponse(response) {
+      this.cities = response.data;
+      return response.data;
+    },
+    fetchForecast() {}
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
